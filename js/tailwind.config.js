4c33fd0da05459f357e36c34f68097db81ff51c9
Gustavo Portilla <gportillagra@gmail.com>
2025-08-27 23:11:26 -0600

    update

---
+++ js/tailwind.config.js
@@ -0,0 +1,30 @@
+// tailwind.config.js
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'md-primary': '#FF5277',
+        'md-on-primary': '#FFFFFF',
+        'md-primary-container': '#FFD9E2',
+        'md-on-primary-container': '#660021',
+        'md-secondary': '#5B4F7B',
+        'md-on-secondary': '#FFFFFF',
+        'md-secondary-container': '#E2D9FF',
+        'md-on-secondary-container': '#1C0D37',
+        'md-background': '#FFFBFE',
+        'md-on-background': '#1D1B1F',
+        'md-surface': '#FFFBFE',
+        'md-on-surface': '#1D1B1F',
+        'md-surface-variant': '#F3DDE3',
+        'md-on-surface-variant': '#504147',
+        'md-outline': '#837078',
+        'md-surface-container-high': '#ECE6F0',
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'oswald': ['Oswald', 'sans-serif'],
+      }
+    }
+  }
+}
