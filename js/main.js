cc74505cd9a79f0746561f5691e7037f5cc0f2f2
Gustavo Portilla <gportillagra@gmail.com>
2025-08-28 19:04:33 -0600

    Actualización emptyState index.html

--- js/main.js
+++ js/main.js
@@ -96,11 +96,11 @@ function applyFilters() {
   });
 
   shuffleInstance.layout();
-  checkEmptyShuffle();
+  
 } 
 
 // ¡CAMBIO! Lógica para manejar resultados vacíos
-function checkEmptyShuffle() {
+/*function checkEmptyShuffle() {
   if (shuffleInstance) {
     if (shuffleInstance.visibleItems.length === 0) {
       noResultsState.classList.remove('hidden');
@@ -110,7 +110,26 @@ function checkEmptyShuffle() {
       catalogGrid.classList.remove('hidden');
     }
   }
+}*/
+
+function checkEmptyShuffle() {
+  if (shuffleInstance) {
+    //const visibleItemsCount = shuffleInstance.visibleItems.length;
+    const visibleItemsCount = shuffleInstance.items.filter(item => item.isVisible).length;
+    console.log("Cantidad de elementos visibles:", visibleItemsCount);
+
+    if (visibleItemsCount === 0) {
+      console.log("No hay resultados, mostrando 'noResultsState' y ocultando 'catalogGrid'");
+      noResultsState.classList.remove('hidden');
+      catalogGrid.classList.add('hidden');
+    } else {
+      console.log("Hay resultados, mostrando 'catalogGrid' y ocultando 'noResultsState'");
+      noResultsState.classList.add('hidden');
+      catalogGrid.classList.remove('hidden');
+    }
+  }
 }
+
 
 function displayCatalogos() {
     const grid = document.getElementById('catalog-grid');
@@ -185,9 +204,12 @@ function displayCatalogos() {
       }
     });
     
-    // ¡CAMBIO! Evento de Shuffle para manejar el fin de la animación
-    shuffleInstance.on(Shuffle.EventType.LAYOUT, checkEmptyShuffle);
-
+    /*shuffleInstance.on(Shuffle.EventType.LAYOUT, function() {
+        checkEmptyShuffle();
+    });*/
+    shuffleInstance.on(Shuffle.EventType.FILTER_END, checkEmptyShuffle);
+shuffleInstance.on(Shuffle.EventType.LAYOUT, checkEmptyShuffle);
+    
     imagesLoaded(grid).on('always', function() {
         grid.style.visibility = 'visible';
         shuffleInstance.layout();
