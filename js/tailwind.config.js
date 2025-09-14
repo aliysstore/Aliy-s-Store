0aad81f73b8639854496dc3515a1d419f261b1a0
Gustavo Portilla <gportillagra@gmail.com>
2025-09-13 21:43:42 -0600

    update

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -24,7 +24,22 @@ tailwind.config = {
       fontFamily: {
         'nunito': ['Nunito', 'sans-serif'],
         'oswald': ['Oswald', 'sans-serif'],
-      }
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'fade-in-up': 'fadeInUp 0.5s ease-out',
+      },
     }
   }
 }
